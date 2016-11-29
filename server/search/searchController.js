fca811a62339b3196938c73b8cbb055ff56f4d52
Adam Wang <adamw3494@gmail.com>
2016-11-29 10:38:42 -0500

    key change

--- server/search/searchController.js
+++ server/search/searchController.js
@@ -1,7 +1,7 @@
 
-var key = require('../config/maps.js');
+var key = process.env.key || require('../config/maps.js');
 var googleMapsClient = require('@google/maps').createClient({
-  key: process.env.key || key
+  key: key
 });
 
 module.exports = {
